40e241b547817cad2f190384fcc06f239ca18086
Anandita kumari <75248882+ananditakumari14@users.noreply.github.com>
2022-08-13 01:16:24 +0530

    Add files via upload

---
+++ Front-end/src/app/viewsoldhistory/viewsoldhistory.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-viewsoldhistory',
+  templateUrl: './viewsoldhistory.component.html',
+  styleUrls: ['./viewsoldhistory.component.css']
+})
+export class ViewsoldhistoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
